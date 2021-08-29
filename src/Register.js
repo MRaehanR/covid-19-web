812bf4599fe781864aadc1037185f2aed667eb27
MRaehanR <raehanrobban2@gmail.com>
2021-08-29 20:35:21 +0700

    [RR] menambahkan halaman register

---
+++ src/Register.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Register extends Component {
+    render() {
+        return (
+            <div>
+                <h1>INI REGISTER</h1>
+            </div>
+        )
+    }
+}
